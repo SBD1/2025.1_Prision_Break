ce4fe9b5395c09b5604ecc803c3bc7dc2b9f72bb
Maliz30 <marialice3003@gmail.com>
2025-06-09 10:15:01 -0300

    Fix: Corrige ordem de criação das tabelas

--- ddl.sql
+++ ddl.sql
@@ -1,41 +1,10 @@
-CREATE TABLE Item (
-    nome_item            VARCHAR(50)  PRIMARY KEY,
-    descricao            VARCHAR (500) NOT NULL,
-    durabilidade         INT           DEFAULT 1,
-    pode_ser_vendido     BOOLEAN       DEFAULT NOT,
-    nome_missao          VARCHAR(255)  NOT NULL,
-    utilidade            VARCHAR (500) DEFAULT NULL,
-    beneficio            VARCHAR(500)  DEFAULT NULL,
+CREATE TABLE Gangue (
+    nome_gangue          VARCHAR(50)   NOT NULL,
+    descricao            VARCHAR(1000) NOT NULL,
+    qtdd_membros         INT           DEFAULT 0,
+    qtdd_recurso_gangue  INT           DEFAULT 0,
 
-    FOREIGN KEY (nome_missao) REFERENCES Missao(nome_missao)
-);
-
-CREATE TABLE Instancia_Item (
-    id_instancia          INT         PRIMARY KEY,
-    nivel_de_gasto        INT         DEFAULT 1,
-    id_inventario         INT         NOT NULL,
-    nome_item             VARCHAR(50) NOT NULL,
-
-    FOREIGN KEY (id_inventario) REFERENCES Inventario(id_inventario),
-    FOREIGN KEY (nome_item) REFERENCES Item(nome_item)
-);
-
-CREATE TABLE Item_Loja (
-    id_compra              INT          PRIMARY KEY,
-    nome_gangue            VARCHAR(50)  NOT NULL,
-    nome_item              VARCHAR(100) NOT NULL,
-
-    FOREIGN KEY (nome_gangue) REFERENCES Gangue(nome_gangue),
-    FOREIGN KEY (nome_item) REFERENCES Item(nome_item)
-);
-
-CREATE TABLE Loja (
-    nome_gangue            VARCHAR(50)     NOT NULL,
-    preco                  INT             NOT NULL,
-    nome_item              VARCHAR(100)    NOT NULL,
-
-    FOREIGN KEY (nome_gangue) REFERENCES Gangue(nome_gangue),
-    FOREIGN KEY (nome_item) REFERENCES Item(nome_item)
+    PRIMARY KEY (nome_gangue)  
 );
 
 CREATE TABLE Inventario (
@@ -55,20 +24,39 @@ CREATE TABLE Sala (
     FOREIGN KEY     (id_inventario) REFERENCES Inventario(id_inventario)
 );
 
-CREATE TABLE Missao_Sala (
-    nome_missao            VARCHAR(255)  NOT NULL,
-    id_sala                INT           NOT NULL,
-    
-    PRIMARY KEY (nome_missao, id_sala), 
-    FOREIGN KEY (id_sala) REFERENCES Sala(id_sala)
+CREATE TABLE Item (
+    nome_item            VARCHAR(50)  PRIMARY KEY,
+    descricao            VARCHAR (500) NOT NULL,
+    durabilidade         INT           DEFAULT 1,
+    pode_ser_vendido     BOOLEAN       DEFAULT NOT,
+    nome_missao          VARCHAR(255)  NOT NULL,
+    utilidade            VARCHAR (500) DEFAULT NULL,
+    beneficio            VARCHAR(500)  DEFAULT NULL,
+
     FOREIGN KEY (nome_missao) REFERENCES Missao(nome_missao)
 );
+
 CREATE TABLE Consulta_Personagem(
     id_personagem        SERIAL      PRIMARY KEY,    
     tipo_personagem      VARCHAR(2)  NOT NULL      	
 );
 
-CREATE TABLE Agente_Penitenciario(
+CREATE TABLE Prisioneiro (
+    id_personagem   INT           NOT NULL,
+    nome            VARCHAR(50)   NOT NULL,
+    velocidade      INT           DEFAULT 0,
+    vida            INT           DEFAULT 0,
+    crime           VARCHAR(255)  DEFAULT NULL,
+    id_sala         INT           NOT NULL,
+    nome_gangue     VARCHAR(50)   NOT NULL,
+
+    PRIMARY KEY (id_personagem),
+    UNIQUE (nome),
+    FOREIGN KEY (id_sala)       REFERENCES Sala (id_sala),
+    FOREIGN KEY (nome_gangue)   REFERENCES Gangue (nome_gangue)
+);
+
+CREATE TABLE Agente_Penitenciario (
     id_personagem          INT         PRIMARY KEY,
     id_sala                INT         NOT NULL,         			
     nome                   VARCHAR(50) DEFAULT 'Tira',
@@ -81,15 +69,6 @@ CREATE TABLE Agente_Penitenciario(
 
     FOREIGN KEY (id_sala) REFERENCES Sala(id_sala), 
     FOREIGN KEY (id_personagem) REFERENCES Consulta_Personagem(id_personagem)
-);
-
-CREATE TABLE Agente_Penitenciario_Jogador(
-    id_captura                         SERIAL PRIMARY KEY,        	
-    id_personagem_jogador              INT,           
-    id_personagem_agente_penitenciario INT,
-
-    FOREIGN KEY (id_personagem_jogador) REFERENCES Jogador(id_personagem),
-    FOREIGN KEY (id_personagem_agente_penitenciario) REFERENCES Agente_Penitenciario(id_personagem)
 );
 
 CREATE TABLE Jogador (
@@ -114,28 +93,50 @@ CREATE TABLE Jogador (
     FOREIGN KEY (nome_gangue)       REFERENCES Gangue (nome_gangue)
 );
 
-CREATE TABLE Prisioneiro (
-    id_personagem   INT           NOT NULL,
-    nome            VARCHAR(50)   NOT NULL,
-    velocidade      INT           DEFAULT 0,
-    vida            INT           DEFAULT 0,
-    crime           VARCHAR(255)  DEFAULT NULL,
-    id_sala         INT           NOT NULL,
-    nome_gangue     VARCHAR(50)   NOT NULL,
+CREATE TABLE Agente_Penitenciario_Jogador(
+    id_captura                         SERIAL PRIMARY KEY,        	
+    id_personagem_jogador              INT,           
+    id_personagem_agente_penitenciario INT,
 
-    PRIMARY KEY (id_personagem),
-    UNIQUE (nome),
-    FOREIGN KEY (id_sala)       REFERENCES Sala (id_sala),
-    FOREIGN KEY (nome_gangue)   REFERENCES Gangue (nome_gangue)
+    FOREIGN KEY (id_personagem_jogador) REFERENCES Jogador(id_personagem),
+    FOREIGN KEY (id_personagem_agente_penitenciario) REFERENCES Agente_Penitenciario(id_personagem)
 );
 
-CREATE TABLE Gangue (
-    nome_gangue          VARCHAR(50)   NOT NULL,
-    descricao            VARCHAR(1000) NOT NULL,
-    qtdd_membros         INT           DEFAULT 0,
-    qtdd_recurso_gangue  INT           DEFAULT 0,
+CREATE TABLE Loja (
+    nome_gangue            VARCHAR(50)     NOT NULL,
+    preco                  INT             NOT NULL,
+    nome_item              VARCHAR(100)    NOT NULL,
 
-    PRIMARY KEY (nome_gangue)  
+    FOREIGN KEY (nome_gangue) REFERENCES Gangue(nome_gangue),
+    FOREIGN KEY (nome_item) REFERENCES Item(nome_item)
+);
+
+CREATE TABLE Item_Loja (
+    id_compra              INT          PRIMARY KEY,
+    nome_gangue            VARCHAR(50)  NOT NULL,
+    nome_item              VARCHAR(100) NOT NULL,
+
+    FOREIGN KEY (nome_gangue) REFERENCES Gangue(nome_gangue),
+    FOREIGN KEY (nome_item) REFERENCES Item(nome_item)
+);
+
+CREATE TABLE Missao_Sala (
+    nome_missao            VARCHAR(255)  NOT NULL,
+    id_sala                INT           NOT NULL,
+    
+    PRIMARY KEY (nome_missao, id_sala), 
+    FOREIGN KEY (id_sala) REFERENCES Sala(id_sala)
+    FOREIGN KEY (nome_missao) REFERENCES Missao(nome_missao)
+);
+
+CREATE TABLE Instancia_Item (
+    id_instancia          INT         PRIMARY KEY,
+    nivel_de_gasto        INT         DEFAULT 1,
+    id_inventario         INT         NOT NULL,
+    nome_item             VARCHAR(50) NOT NULL,
+
+    FOREIGN KEY (id_inventario) REFERENCES Inventario(id_inventario),
+    FOREIGN KEY (nome_item) REFERENCES Item(nome_item)
 );
 
 -- Apaga tabelas E colunas
