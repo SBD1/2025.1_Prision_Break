79e7fecf7bf81f2042a1f6b6c760c03bb49a0e26
Maliz30 <marialice3003@gmail.com>
2025-06-11 21:24:58 -0300

    refactor: corrige nome errado de arquivo

--- DQL/consulta_peronagem.sql
+++
@@ -1,7 +0,0 @@
--- Lista todos os personagens cadastrados
-SELECT * FROM consulta_personagem
-
--- Exibe o tipo de um personagem específico
-SELECT tipo_personagem
-FROM consulta_personagem
-WHERE id_personagem = 5
