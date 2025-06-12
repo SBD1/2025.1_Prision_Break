e3585393828df786cb36675aca6c6c6b6d7bd5f9
m4rllon <marllonpc33@gmail.com>
2025-06-12 17:38:00 -0300

    feat: Atualizar schemas e queries do container docker

--- game/sql/01_schema.sql
+++ game/sql/01_schema.sql
@@ -1,169 +1,156 @@
-CREATE TABLE Gangue (
+CREATE TABLE IF NOT EXISTS Gangue (
     nome_gangue          VARCHAR(50)   NOT NULL,
     descricao            VARCHAR(1000) NOT NULL,
     qtdd_membros         INT           DEFAULT 0,
     qtdd_recurso_gangue  INT           DEFAULT 0,
-
-    PRIMARY KEY (nome_gangue)  
+    PRIMARY KEY (nome_gangue)
 );
 
-CREATE TABLE Inventario (
-    id_inventario    INT      PRIMARY KEY,
-    qtd_itens        INT      NOT NULL,
-    is_full          BOOLEAN  DEFAULT FALSE
+CREATE TABLE IF NOT EXISTS Inventario (
+    id_inventario        INT           PRIMARY KEY,
+    qtd_itens            INT           NOT NULL,
+    is_full              BOOLEAN       DEFAULT FALSE
 );
 
-CREATE TABLE Objetivo_Principal(
-    titulo_objetivo     VARCHAR(255)    PRIMARY KEY,
-    descricao           VARCHAR(1000)   NOT NULL
+CREATE TABLE IF NOT EXISTS Objetivo_Principal (
+    titulo_objetivo      VARCHAR(255)  PRIMARY KEY,
+    descricao            VARCHAR(1000) NOT NULL
 );
 
-CREATE TABLE Missao(
-    nome_missao        VARCHAR(255)   PRIMARY KEY,
-    descricao          VARCHAR(1000)  NOT NULL,
-    status             BOOLEAN        DEFAULT false
+CREATE TABLE IF NOT EXISTS Missao (
+    nome_missao          VARCHAR(255)  PRIMARY KEY,
+    descricao            VARCHAR(1000) NOT NULL,
+    status               BOOLEAN       DEFAULT FALSE
 );
 
-CREATE TABLE Sala (
-    id_sala         INT           PRIMARY KEY,
-    id_inventario   INT           NOT NULL,
-    nome            VARCHAR(50)   NOT NULL,
-    descricao       VARCHAR(1000) NOT NULL,
-    nivel_perigo    INT           CHECK (nivel_perigo >= 0 AND nivel_perigo <= 10),
-    bloqueado       BOOLEAN       DEFAULT FALSE,
-
-    FOREIGN KEY     (id_inventario) REFERENCES Inventario(id_inventario)
+CREATE TABLE IF NOT EXISTS Consulta_Personagem (
+    id_personagem        SERIAL        PRIMARY KEY,
+    tipo_personagem      VARCHAR(2)    NOT NULL
 );
 
-CREATE TABLE Item (
-    nome_item            VARCHAR(50)  PRIMARY KEY,
-    descricao            VARCHAR (500) NOT NULL,
+CREATE TABLE IF NOT EXISTS Sala (
+    id_sala              INT           PRIMARY KEY,
+    id_inventario        INT           NOT NULL,
+    nome                 VARCHAR(50)   NOT NULL,
+    descricao            VARCHAR(1000) NOT NULL,
+    nivel_perigo         INT           CHECK (nivel_perigo >= 0 AND nivel_perigo <= 10),
+    bloqueado            BOOLEAN       DEFAULT FALSE,
+    FOREIGN KEY (id_inventario) REFERENCES Inventario(id_inventario)
+);
+
+CREATE TABLE IF NOT EXISTS Item (
+    nome_item            VARCHAR(50)   PRIMARY KEY,
+    descricao            VARCHAR(500)  NOT NULL,
     durabilidade         INT           DEFAULT 1,
     pode_ser_vendido     BOOLEAN       DEFAULT FALSE,
     nome_missao          VARCHAR(255)  NOT NULL,
-    utilidade            VARCHAR (500) DEFAULT NULL,
+    utilidade            VARCHAR(500)  DEFAULT NULL,
     beneficio            VARCHAR(500)  DEFAULT NULL,
-
     FOREIGN KEY (nome_missao) REFERENCES Missao(nome_missao)
 );
 
-CREATE TABLE Consulta_Personagem(
-    id_personagem        SERIAL      PRIMARY KEY,    
-    tipo_personagem      VARCHAR(2)  NOT NULL      	
-);
-
-CREATE TABLE Prisioneiro (
-    id_personagem   INT           NOT NULL,
-    nome            VARCHAR(50)   NOT NULL,
-    velocidade      INT           DEFAULT 0,
-    vida            INT           DEFAULT 0,
-    crime           VARCHAR(255)  DEFAULT NULL,
-    id_sala         INT           NOT NULL,
-    nome_gangue     VARCHAR(50)   NOT NULL,
-
+CREATE TABLE IF NOT EXISTS Prisioneiro (
+    id_personagem        INT           NOT NULL,
+    nome                 VARCHAR(50)   NOT NULL,
+    velocidade           INT           DEFAULT 0,
+    vida                 INT           DEFAULT 0,
+    crime                VARCHAR(255)  DEFAULT NULL,
+    id_sala              INT           NOT NULL,
+    nome_gangue          VARCHAR(50)   NOT NULL,
     PRIMARY KEY (id_personagem),
     UNIQUE (nome),
-    FOREIGN KEY (id_sala)       REFERENCES Sala (id_sala),
-    FOREIGN KEY (nome_gangue)   REFERENCES Gangue (nome_gangue)
+    FOREIGN KEY (id_sala) REFERENCES Sala (id_sala),
+    FOREIGN KEY (nome_gangue) REFERENCES Gangue (nome_gangue)
 );
 
-CREATE TABLE Agente_Penitenciario (
-    id_personagem          INT         PRIMARY KEY,
-    id_sala                INT         NOT NULL,         			
-    nome                   VARCHAR(50) DEFAULT 'Tira',
-    velocidade             INT         DEFAULT 5,
-    nivel_de_perigo        INT         DEFAULT 5,
-    nivel_de_alerta        INT         DEFAULT 5,
-    corrupto               BOOLEAN     DEFAULT false, 
-    preco                  INT         DEFAULT 0,
-    cargo                 VARCHAR(255) DEFAULT 'Carcereiro',
-
+CREATE TABLE IF NOT EXISTS Agente_Penitenciario (
+    id_personagem        INT           PRIMARY KEY,
+    id_sala              INT           NOT NULL,
+    nome                 VARCHAR(50)   DEFAULT 'Tira',
+    velocidade           INT           DEFAULT 5,
+    nivel_de_perigo      INT           DEFAULT 5,
+    nivel_de_alerta      INT           DEFAULT 5,
+    corrupto             BOOLEAN       DEFAULT FALSE,
+    preco                INT           DEFAULT 0,
+    cargo                VARCHAR(255)  DEFAULT 'Carcereiro',
     FOREIGN KEY (id_sala) REFERENCES Sala(id_sala)
 );
 
-CREATE TABLE Jogador (
-    id_personagem     INT           NOT NULL,
-    nome              VARCHAR(50)   NOT NULL,
-    velocidade        INT           DEFAULT 0,
-    vida              INT           DEFAULT 0,
-    qtded_recurso     INT           DEFAULT 0,
-    qtded_captura     INT           DEFAULT 0,
-    id_sala           INT           NOT NULL,
-    id_inventario     INT           NOT NULL,
-    nome_missao       VARCHAR(255)  NOT NULL,
-    titulo_objetivo   VARCHAR(255)  NOT NULL,
-    nome_gangue       VARCHAR(50)   NOT NULL,
-
+CREATE TABLE IF NOT EXISTS Jogador (
+    id_personagem        INT           NOT NULL,
+    nome                 VARCHAR(50)   NOT NULL,
+    velocidade           INT           DEFAULT 0,
+    vida                 INT           DEFAULT 0,
+    qtded_recurso        INT           DEFAULT 0,
+    qtded_captura        INT           DEFAULT 0,
+    id_sala              INT           NOT NULL,
+    id_inventario        INT           NOT NULL,
+    nome_missao          VARCHAR(255)  NOT NULL,
+    titulo_objetivo      VARCHAR(255)  NOT NULL,
+    nome_gangue          VARCHAR(50)   NOT NULL,
     PRIMARY KEY (id_personagem),
     UNIQUE (nome),
-    FOREIGN KEY (id_sala)           REFERENCES Sala (id_sala),
-    FOREIGN KEY (id_inventario)     REFERENCES Inventario (id_inventario),
-    FOREIGN KEY (nome_missao)       REFERENCES Missao (nome_missao),
-    FOREIGN KEY (titulo_objetivo)   REFERENCES Objetivo_principal (titulo_objetivo),
-    FOREIGN KEY (nome_gangue)       REFERENCES Gangue (nome_gangue)
+    FOREIGN KEY (id_sala) REFERENCES Sala (id_sala),
+    FOREIGN KEY (id_inventario) REFERENCES Inventario (id_inventario),
+    FOREIGN KEY (nome_missao) REFERENCES Missao (nome_missao),
+    FOREIGN KEY (titulo_objetivo) REFERENCES Objetivo_Principal (titulo_objetivo),
+    FOREIGN KEY (nome_gangue) REFERENCES Gangue (nome_gangue)
 );
 
-CREATE TABLE Agente_Penitenciario_Jogador(
-    id_captura                         SERIAL PRIMARY KEY,        	
-    id_personagem_jogador              INT,           
+CREATE TABLE IF NOT EXISTS Agente_Penitenciario_Jogador (
+    id_captura                      SERIAL        PRIMARY KEY,
+    id_personagem_jogador           INT,
     id_personagem_agente_penitenciario INT,
-
     FOREIGN KEY (id_personagem_jogador) REFERENCES Jogador(id_personagem),
     FOREIGN KEY (id_personagem_agente_penitenciario) REFERENCES Agente_Penitenciario(id_personagem)
 );
 
-CREATE TABLE Loja (
-    nome_gangue            VARCHAR(50)     NOT NULL,
-    preco                  INT             NOT NULL,
-    nome_item              VARCHAR(100)    NOT NULL,
-
+CREATE TABLE IF NOT EXISTS Loja (
+    nome_gangue          VARCHAR(50)   NOT NULL,
+    preco                INT           NOT NULL,
+    nome_item            VARCHAR(100)  NOT NULL,
     FOREIGN KEY (nome_gangue) REFERENCES Gangue(nome_gangue),
     FOREIGN KEY (nome_item) REFERENCES Item(nome_item)
 );
 
-CREATE TABLE Item_Loja (
-    id_compra              INT          PRIMARY KEY,
-    nome_gangue            VARCHAR(50)  NOT NULL,
-    nome_item              VARCHAR(100) NOT NULL,
-
+CREATE TABLE IF NOT EXISTS Item_Loja (
+    id_compra            INT           PRIMARY KEY,
+    nome_gangue          VARCHAR(50)   NOT NULL,
+    nome_item            VARCHAR(100)  NOT NULL,
     FOREIGN KEY (nome_gangue) REFERENCES Gangue(nome_gangue),
     FOREIGN KEY (nome_item) REFERENCES Item(nome_item)
 );
 
-CREATE TABLE Missao_Sala (
-    nome_missao            VARCHAR(255)  NOT NULL,
-    id_sala                INT           NOT NULL,
-    
-    PRIMARY KEY (nome_missao, id_sala), 
+CREATE TABLE IF NOT EXISTS Missao_Sala (
+    nome_missao          VARCHAR(255)  NOT NULL,
+    id_sala              INT           NOT NULL,
+    PRIMARY KEY (nome_missao, id_sala),
     FOREIGN KEY (id_sala) REFERENCES Sala(id_sala),
     FOREIGN KEY (nome_missao) REFERENCES Missao(nome_missao)
 );
 
-CREATE TABLE Instancia_Item (
-    id_instancia          INT         PRIMARY KEY,
-    nivel_de_gasto        INT         DEFAULT 1,
-    id_inventario         INT         NOT NULL,
-    nome_item             VARCHAR(50) NOT NULL,
-
+CREATE TABLE IF NOT EXISTS Instancia_Item (
+    id_instancia         INT           PRIMARY KEY,
+    nivel_de_gasto       INT           DEFAULT 1,
+    id_inventario        INT           NOT NULL,
+    nome_item            VARCHAR(50)   NOT NULL,
     FOREIGN KEY (id_inventario) REFERENCES Inventario(id_inventario),
     FOREIGN KEY (nome_item) REFERENCES Item(nome_item)
 );
 
-CREATE TABLE Dialogo(
-    id_dialogo       INT     PRIMARY KEY,
-    id_personagem    INT,
-    nome_missao      VARCHAR(255),
-    texto            VARCHAR (500),
-    ordem            INT,
-
+CREATE TABLE IF NOT EXISTS Dialogo (
+    id_dialogo           INT           PRIMARY KEY,
+    id_personagem        INT,
+    nome_missao          VARCHAR(255),
+    texto                VARCHAR(500),
+    ordem                INT,
     FOREIGN KEY (id_personagem) REFERENCES Consulta_Personagem(id_personagem),
     FOREIGN KEY (nome_missao) REFERENCES Missao(nome_missao)
 );
 
-CREATE TABLE Objetivo_principal_missao (
-    titulo_objetivo     VARCHAR(255)    NOT NULL,
-    nome_missao        VARCHAR(255)     NOT NULL,
-
+CREATE TABLE IF NOT EXISTS Objetivo_principal_missao (
+    titulo_objetivo      VARCHAR(255)  NOT NULL,
+    nome_missao          VARCHAR(255)  NOT NULL,
     FOREIGN KEY (titulo_objetivo) REFERENCES Objetivo_Principal(titulo_objetivo),
     FOREIGN KEY (nome_missao) REFERENCES Missao(nome_missao)
 );
--- game/sql/02_data.sql
+++ game/sql/02_data.sql
@@ -20,7 +20,15 @@ INSERT INTO sala (id_sala, id_inventario, nome, descricao, nivel_perigo, bloquea
     (4, 5, 'Sala para telefonemas', 'Local onde os detentos podem fazer ligações.', 5, FALSE);
 
 INSERT INTO Missao (nome_missao, descricao, status) VALUES
-    ('Fuga', 'Fuja da prisão.', false);
+    ('Fuga', 'Fuja da prisão.', false),
+    ('Missão de Confiança', 'Ganhar a confiança de um membro influente da máfia para obter recursos essenciais', false),
+    ('Aliança Perigosa', 'Fazer uma aliança temporária com uma gangue rival para obter acesso a áreas restritas', false),
+    ('Túnel da Liberdade', 'Construir um túnel secreto que leve para fora dos muros da prisão', false),
+    ('Informante Infiltrado', 'Descobrir e neutralizar um informante que está vazando informações para os guardas', false),
+    ('Distração Programada', 'Criar um incidente que distraia a atenção dos guardas no momento crítico da fuga', false),
+    ('Fuga de Fox River', 'Planejar e executar a fuga da penitenciária Fox River, envolvendo múltiplos prisioneiros e etapas complexas', false),
+    ('Rota Segura', 'Estabelecer um caminho seguro após a fuga, evitando captura pelos agentes penitenciários', false),
+    ('Arquivos da Sona', 'Obter documentos secretos que provam a conspiração contra Lincoln Burrows', false);
 
 -- Inserindo em Item -> Tem que ser criada após missão
 INSERT INTO Item (nome_item, descricao, durabilidade, pode_ser_vendido, nome_missao, utilidade, beneficio) VALUES
@@ -36,42 +44,21 @@ INSERT INTO Instancia_Item (id_instancia, nivel_de_gasto, id_inventario, nome_it
 
 -- Inserindo em Item_Loja
 INSERT INTO Item_Loja (id_compra, nome_gangue, nome_item) VALUES
-(1, 'Mafia Abruzzi', 'Chave Inglesa'),
-(2, 'Irmandade Ariana', 'Lanterna'),
-(3, 'La Familia', 'Mapa');
+    (1, 'Mafia Abruzzi', 'Chave Inglesa'),
+    (2, 'Irmandade Ariana', 'Lanterna'),
+    (3, 'La Familia', 'Mapa');
 
 -- Inserindo em Loja
 INSERT INTO Loja (nome_gangue, preco, nome_item) VALUES
-('Mafia Abruzzi', 150.00, 'Chave Inglesa'),
-('Irmandade Ariana', 75.50, 'Lanterna'),
-('La Familia', 200.00, 'Mapa');
+    ('Mafia Abruzzi', 150.00, 'Chave Inglesa'),
+    ('Irmandade Ariana', 75.50, 'Lanterna'),
+    ('La Familia', 200.00, 'Mapa');
 
-INSERT INTO Objetivo_Principal (titulo_objetivo, descricao) VALUES
-('Escapar da Prisão', 'Encontrar uma maneira de sair da prisão sem ser detectado pelos guardas.'),
-('Coletar Recursos', 'Reunir itens essenciais como ferramentas, mapas e suprimentos para a fuga.'),
-('Desativar Alarmes', 'Localizar e desativar os sistemas de alarme da prisão para garantir uma saída silenciosa.'),
-('Subornar Guarda', 'Conseguir informações ou ajuda de um guarda corrupto oferecendo algo em troca.');
-
-INSERT INTO Jogador (id_personagem, nome, velocidade, vida, qtded_recurso, qtded_captura, id_sala, id_inventario, nome_missao, titulo_objetivo, nome_gangue) VALUES
-(1, 'Michael Scofield', 10, 100, 50, 0, 1, 4, 'Fuga', 'Escapar da Prisão', 'Mafia Abruzzi');
-
-INSERT INTO Prisioneiro (id_personagem, nome, velocidade, vida, crime, id_sala, nome_gangue) VALUES
-(1001, 'João Ligeiro', 5, 80, 'Pequeno Furto', 2, 'Mafia Abruzzi'),
-(1002, 'Maria Silenciosa', 7, 75, 'Invasão', 2, 'Mafia Abruzzi'),
-(1003, 'Carlos Esperto', 6, 90, 'Fraude', 2, 'Mafia Abruzzi'),
-(1004, 'Ana Forte', 4, 110, 'Agressão', 2, 'Irmandade Ariana'),
-(1005, 'Pedro Sombra', 8, 70, 'Evasão Fiscal', 2, 'Mafia Abruzzi'),
-(1006, 'Sofia Vigilante', 5, 85, 'Vandalismo', 2, 'Mafia Abruzzi'),
-(1007, 'Marcelo Desenhista', 3, 60, 'Falsificação', 2, 'La Familia'),
-(1008, 'Beatriz Rápida', 9, 95, 'Roubo de Informações', 2, 'La Familia'),
-(1009, 'Fernando Mudo', 2, 50, 'Contrabando', 2, 'La Familia'),
-(1010, 'Gabriela Ágil', 7, 88, 'Sequestro', 2, 'Irmandade Ariana');
-
--- Consulta_Personagem
-INSERT INTO Consulta_Personagem (tipo_personagem) VALUES
-('J'),
-('P'),
-('AP');
+INSERT INTO Consulta_Personagem (id_personagem, tipo_personagem) VALUES
+    (8, 'PR'),
+    (9, 'PR'), 
+    (10, 'PR'), 
+    (11, 'PR'); 
 
 -- Agente_Penitenciario
 INSERT INTO Agente_Penitenciario(
@@ -91,11 +78,54 @@ INSERT INTO Agente_Penitenciario(
     (4, 2, 'Agente D', 3, 4, 4, true, 10, 'Guarda'),
     (5, 2, 'Agente E', 10, 8, 8, false, 0, 'Diretor');
 
--- Inserindo em Agente_Penitenciario_Jogador
-INSERT INTO Agente_Penitenciario_Jogador(
-    -- id_personagem_jogador,		--Descomentar depois de adicionar tabela de Jogador 
-    id_personagem_agente_penitenciario 
-) VALUES
-	(1);
--- (1, 2); 
+INSERT INTO Prisioneiro (id_personagem, nome, velocidade, vida, crime, id_sala, nome_gangue) VALUES
+    (1001, 'João Ligeiro', 5, 80, 'Pequeno Furto', 2, 'Mafia Abruzzi'),
+    (1002, 'Maria Silenciosa', 7, 75, 'Invasão', 2, 'Mafia Abruzzi'),
+    (1003, 'Carlos Esperto', 6, 90, 'Fraude', 2, 'Mafia Abruzzi'),
+    (1004, 'Ana Forte', 4, 110, 'Agressão', 2, 'Irmandade Ariana'),
+    (1005, 'Pedro Sombra', 8, 70, 'Evasão Fiscal', 2, 'Mafia Abruzzi'),
+    (1006, 'Sofia Vigilante', 5, 85, 'Vandalismo', 2, 'Mafia Abruzzi'),
+    (1007, 'Marcelo Desenhista', 3, 60, 'Falsificação', 2, 'La Familia'),
+    (1008, 'Beatriz Rápida', 9, 95, 'Roubo de Informações', 2, 'La Familia'),
+    (1009, 'Fernando Mudo', 2, 50, 'Contrabando', 2, 'La Familia'),
+    (1010, 'Gabriela Ágil', 7, 88, 'Sequestro', 2, 'Irmandade Ariana');
+INSERT INTO Dialogo (id_dialogo, id_personagem, nome_missao, texto, ordem) VALUES
+    (1, 8, 'Fuga de Fox River', 'Lincoln, eu vou te tirar daqui. Confie em mim.', 1),
+    (2, 8, 'Fuga de Fox River', 'Precisamos do plano exato da penitenciária. Alguém tem acesso aos arquivos?', 2),
+    (3, 11, 'Fuga de Fox River', 'Michael, você tá maluco? Eles vão nos pegar!', 3),
+    (4, 8, 'Fuga de Fox River', 'Cada segundo conta. O túnel deve estar pronto em 72 horas.', 4),
+    (5, 9, 'Fuga de Fox River', 'Você tem meu avião, Scofield? Sem avião, sem ajuda.', 5),
+    (6, 9, 'Missão de Confiança', 'Na minha família, lealdade se prova com sangue.', 1),
+    (7, 8, 'Missão de Confiança', 'O que devo fazer para ganhar sua confiança, Abruzzi?', 2),
+    (8, 9, 'Missão de Confiança', 'Traga-me a cabeça do traidor da ala oeste. Literalmente.', 3);
 
+INSERT INTO Objetivo_Principal (titulo_objetivo, descricao) VALUES
+    ('Escapar da Prisão', 'Encontrar uma maneira de sair da prisão sem ser detectado pelos guardas.'),
+    ('Coletar Recursos', 'Reunir itens essenciais como ferramentas, mapas e suprimentos para a fuga.'),
+    ('Desativar Alarmes', 'Localizar e desativar os sistemas de alarme da prisão para garantir uma saída silenciosa.'),
+    ('Subornar Guarda', 'Conseguir informações ou ajuda de um guarda corrupto oferecendo algo em troca.'),
+    ('Libertar Lincoln', 'Objetivo principal de Michael Scofield: provar a inocência e libertar seu irmão Lincoln Burrows da prisão'),
+    ('Sobreviver até a fuga', 'Manter-se vivo e fora do radar dos guardas até o momento da fuga planejada'),
+    ('Proteger a família', 'Garantir a segurança da família enquanto o plano de fuga está em andamento'),
+    ('Provar lealdade', 'Ganhar a confiança de membros-chave da gangue para obter ajuda na fuga'),
+    ('Descobrir plano rival', 'Identificar e neutralizar planos de outras gangues que possam interferir na fuga'),
+    ('Coletar recursos', 'Obter itens essenciais para a fuga, como ferramentas, uniformes e informações'),
+    ('Distrair guardas', 'Criar distrações para permitir que partes críticas do plano sejam executadas'),
+    ('Encontrar Sara', 'Localizar e resgatar a Dra. Sara Tancredi, aliada crucial para o plano de fuga');
+
+INSERT INTO Jogador (id_personagem, nome, velocidade, vida, qtded_recurso, qtded_captura, id_sala, id_inventario, nome_missao, titulo_objetivo, nome_gangue) VALUES
+    (1, 'Michael Scofield', 10, 100, 50, 0, 1, 4, 'Fuga', 'Escapar da Prisão', 'Mafia Abruzzi');
+
+
+INSERT INTO Objetivo_Principal_Missao (titulo_objetivo, nome_missao) VALUES
+	('Provar lealdade', 'Missão de Confiança'),
+    ('Coletar recursos', 'Aliança Perigosa'),
+    ('Sobreviver até a fuga', 'Túnel da Liberdade'),
+    ('Descobrir plano rival', 'Informante Infiltrado'),
+    ('Distrair guardas', 'Distração Programada'),
+    ('Encontrar Sara', 'Fuga de Fox River'),
+    ('Proteger a família', 'Rota Segura'),
+    ('Libertar Lincoln', 'Arquivos da Sona');
+
+INSERT INTO Missao_Sala (nome_missao, id_sala) VALUES
+	('Rota Segura', 3);
