ba4840bac46b00175bf542fd1c94309e9cdd0c64
renannOgomes <renannpop11@gmail.com>
2025-06-13 21:22:15 -0300

    Att tabelas de create

--- game/sql/01_schema.sql
+++ game/sql/01_schema.sql
@@ -50,51 +50,55 @@ CREATE TABLE IF NOT EXISTS Item (
 );
 
 CREATE TABLE IF NOT EXISTS Prisioneiro (
-    id_personagem        INT           NOT NULL,
-    nome                 VARCHAR(50)   NOT NULL,
-    velocidade           INT           DEFAULT 0,
-    vida                 INT           DEFAULT 0,
-    crime                VARCHAR(255)  DEFAULT NULL,
-    id_sala              INT           NOT NULL,
-    nome_gangue          VARCHAR(50)   NOT NULL,
-    PRIMARY KEY (id_personagem),
+    id_personagem   INT           NOT NULL,
+    nome            VARCHAR(50)   NOT NULL,
+    velocidade      INT           DEFAULT 0,
+    vida            INT           DEFAULT 0,
+    crime           VARCHAR(255)  DEFAULT NULL,
+    id_sala         INT           NOT NULL,
+    nome_gangue     VARCHAR(50)   NOT NULL,
+    FOREIGN KEY (id_personagem) REFERENCES Consulta_Personagem(id_personagem),
+    UNIQUE (id_personagem),
     UNIQUE (nome),
-    FOREIGN KEY (id_sala) REFERENCES Sala (id_sala),
-    FOREIGN KEY (nome_gangue) REFERENCES Gangue (nome_gangue)
+    FOREIGN KEY (id_sala)       REFERENCES Sala (id_sala),
+    FOREIGN KEY (nome_gangue)   REFERENCES Gangue (nome_gangue)
 );
 
 CREATE TABLE IF NOT EXISTS Agente_Penitenciario (
-    id_personagem        INT           PRIMARY KEY,
-    id_sala              INT           NOT NULL,
-    nome                 VARCHAR(50)   DEFAULT 'Tira',
-    velocidade           INT           DEFAULT 5,
-    nivel_de_perigo      INT           DEFAULT 5,
-    nivel_de_alerta      INT           DEFAULT 5,
-    corrupto             BOOLEAN       DEFAULT FALSE,
-    preco                INT           DEFAULT 0,
-    cargo                VARCHAR(255)  DEFAULT 'Carcereiro',
+    id_personagem          INT         NOT NULL,
+    id_sala                INT         NOT NULL,         			
+    nome                   VARCHAR(50) DEFAULT 'Tira',
+    velocidade             INT         DEFAULT 5,
+    nivel_de_perigo        INT         DEFAULT 5,
+    nivel_de_alerta        INT         DEFAULT 5,
+    corrupto               BOOLEAN     DEFAULT false, 
+    preco                  INT         DEFAULT 0,
+    cargo                 VARCHAR(255) DEFAULT 'Carcereiro',
+    FOREIGN KEY (id_personagem) REFERENCES Consulta_Personagem(id_personagem),
+    UNIQUE (id_personagem),
     FOREIGN KEY (id_sala) REFERENCES Sala(id_sala)
 );
 
 CREATE TABLE IF NOT EXISTS Jogador (
-    id_personagem        INT           NOT NULL,
-    nome                 VARCHAR(50)   NOT NULL,
-    velocidade           INT           DEFAULT 0,
-    vida                 INT           DEFAULT 0,
-    qtded_recurso        INT           DEFAULT 0,
-    qtded_captura        INT           DEFAULT 0,
-    id_sala              INT           NOT NULL,
-    id_inventario        INT           NOT NULL,
-    nome_missao          VARCHAR(255)  NOT NULL,
-    titulo_objetivo      VARCHAR(255)  NOT NULL,
-    nome_gangue          VARCHAR(50)   NOT NULL,
-    PRIMARY KEY (id_personagem),
+    id_personagem     INT           NOT NULL,
+    id_sala           INT           NOT NULL,
+    id_inventario     INT           NOT NULL,
+    nome_missao       VARCHAR(255),    
+    titulo_objetivo   VARCHAR(255),     
+    nome_gangue       VARCHAR(50),      
+    nome              VARCHAR(50)   NOT NULL,
+    velocidade        INT           DEFAULT 0,
+    vida              INT           DEFAULT 0,
+    qtded_recurso     INT           DEFAULT 0,
+    qtded_captura     INT           DEFAULT 0,
+    UNIQUE (id_personagem),
     UNIQUE (nome),
-    FOREIGN KEY (id_sala) REFERENCES Sala (id_sala),
-    FOREIGN KEY (id_inventario) REFERENCES Inventario (id_inventario),
-    FOREIGN KEY (nome_missao) REFERENCES Missao (nome_missao),
-    FOREIGN KEY (titulo_objetivo) REFERENCES Objetivo_Principal (titulo_objetivo),
-    FOREIGN KEY (nome_gangue) REFERENCES Gangue (nome_gangue)
+    FOREIGN KEY (id_personagem) REFERENCES Consulta_Personagem(id_personagem),
+    FOREIGN KEY (id_sala)           REFERENCES Sala (id_sala),
+    FOREIGN KEY (id_inventario)     REFERENCES Inventario (id_inventario),
+    FOREIGN KEY (nome_missao)       REFERENCES Missao (nome_missao),
+    FOREIGN KEY (titulo_objetivo)   REFERENCES Objetivo_principal (titulo_objetivo),
+    FOREIGN KEY (nome_gangue)       REFERENCES Gangue (nome_gangue)
 );
 
 CREATE TABLE IF NOT EXISTS Agente_Penitenciario_Jogador (
@@ -109,15 +113,16 @@ CREATE TABLE IF NOT EXISTS Loja (
     nome_gangue          VARCHAR(50)   NOT NULL,
     preco                INT           NOT NULL,
     nome_item            VARCHAR(100)  NOT NULL,
+    UNIQUE (nome_gangue),
     FOREIGN KEY (nome_gangue) REFERENCES Gangue(nome_gangue),
     FOREIGN KEY (nome_item) REFERENCES Item(nome_item)
 );
 
 CREATE TABLE IF NOT EXISTS Item_Loja (
-    id_compra            INT           PRIMARY KEY,
-    nome_gangue          VARCHAR(50)   NOT NULL,
-    nome_item            VARCHAR(100)  NOT NULL,
-    FOREIGN KEY (nome_gangue) REFERENCES Gangue(nome_gangue),
+    id_compra              INT          PRIMARY KEY,
+    nome_gangue            VARCHAR(50)  NOT NULL,
+    nome_item              VARCHAR(100) NOT NULL,
+    FOREIGN KEY (nome_gangue) REFERENCES Loja(nome_gangue),
     FOREIGN KEY (nome_item) REFERENCES Item(nome_item)
 );
 
@@ -139,13 +144,13 @@ CREATE TABLE IF NOT EXISTS Instancia_Item (
 );
 
 CREATE TABLE IF NOT EXISTS Dialogo (
-    id_dialogo           INT           PRIMARY KEY,
-    id_personagem        INT,
-    nome_missao          VARCHAR(255),
-    texto                VARCHAR(500),
-    ordem                INT,
-    FOREIGN KEY (id_personagem) REFERENCES Consulta_Personagem(id_personagem),
-    FOREIGN KEY (nome_missao) REFERENCES Missao(nome_missao)
+    id_dialogo       INT            PRIMARY KEY,
+    id_personagem    INT,
+    nome_missao      VARCHAR(255),
+    texto            VARCHAR (500)  NOT NULL,
+    ordem            INT            NOT NULL,
+    FOREIGN KEY (id_personagem)     REFERENCES Consulta_Personagem(id_personagem),
+    FOREIGN KEY (nome_missao)       REFERENCES Missao(nome_missao)
 );
 
 CREATE TABLE IF NOT EXISTS Objetivo_principal_missao (
