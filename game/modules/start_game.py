e4f87706f12fe6b1f6716582ba0fb22922727eba
renannOgomes <renannpop11@gmail.com>
2025-07-07 23:18:21 -0300

    Adicionando desc missao + selecionar gangue eh obg

--- game/modules/start_game.py
+++ game/modules/start_game.py
@@ -25,13 +25,15 @@ def start_game(conn, cursor):
                 print("Erro ao carregar dados do jogador.")
                 break
 
-            id_personagem, nome, qtded_recurso, gangue, titulo_objetivo, nome_missao = dados_jogador
+            id_personagem, nome, qtded_recurso, gangue, titulo_objetivo, nome_missao, missao_desc = dados_jogador
 
             print(f"==========================================================================================")
             print(f"[ PRISON BREAK ] {nome} | Recursos: {qtded_recurso} | Gangue: {gangue or 'Nenhuma'}")
             print(f"-------------------------------------------------------------------------------------------")
             print(f"OBJETIVO: {titulo_objetivo}")
             print(f"MISSÃO ATUAL: {nome_missao}")
+            print(f"-------------------------------------------------------------------------------------------")
+            print(f"DESCRICAO: {missao_desc}")
             print(f"-------------------------------------------------------------------------------------------")
 
             # Sala atual do jogador
--- game/sql_queries/select_player_data.sql
+++ game/sql_queries/select_player_data.sql
@@ -1,4 +1,5 @@
-SELECT J.id_personagem, J.nome, J.qtded_recurso, G.nome_gangue AS gangue, J.titulo_objetivo, J.nome_missao
+SELECT J.id_personagem, J.nome, J.qtded_recurso, G.nome_gangue AS gangue, J.titulo_objetivo, J.nome_missao,  M.descricao 
 FROM Jogador J
 JOIN gangue G ON J.nome_gangue = G.nome_gangue
+JOIN missao M ON J.nome_missao = M.nome_missao
 WHERE J.id_personagem = %s
